34de50919dd257503731e6ce164ffbca9dc9c7b0
bgarcia95 <bgarcia.95@outlook.com>
2022-12-15 22:59:53 -0600

    use for in loop

---
+++ 04_for_in.dart
@@ -0,0 +1,11 @@
+main() {
+  List<String> list = ['Batman', 'Superman', 'Wonder Woman'];
+
+  // for (int i = 0; i < list.length; i++) {
+  //   print(list[i]);
+  // }
+
+  for (String superhero in list) {
+    print(superhero);
+  }
+}
